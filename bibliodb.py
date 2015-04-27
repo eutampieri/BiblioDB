1eb5f37c7ad9fe0a20ddecc2908837b48f85853a
eutampieri <eugenio.tampieri@icloud.com>
2015-04-27 18:29:37 +0200

    Version 1.0.4

--- bibliodb.py
+++ bibliodb.py
@@ -50,6 +50,26 @@ finally:
             o)
         o.close
 
+def add(titolo, isbn, autore, pos):
+    isbnPos[isbn] = pos
+    titleIsbn[titolo] = isbn
+    isbnTitle[isbn] = titolo
+    isbnAuthor[isbn] = autore
+    ISBNown[isbn] = "Biblioteca"
+    o = open('bibliodb.json', 'w')
+    json.dump(
+        (ISBNuse,
+         isbnPos,
+         titleIsbn,
+         isbnTitle,
+         isbnAuthor,
+         nomeFile,
+         ISBNown,
+         borrowTime,
+         ISBNborrowDate),
+        o)
+    o.close()
+    pass
 
 def CLIadd():
     titolo = raw_input("Titolo: ").lower()
