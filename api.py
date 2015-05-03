50de52cb7036ab0e073bad77177b591d327177b1
eutampieri <eugenio@serverseutampieri.ddns.net>
2015-05-03 17:20:08 +0200

    Version 1.1.2
    Fixed a bug in api.py

--- api.py
+++ api.py
@@ -330,7 +330,7 @@ def tsvGetter():
 	return resp
 @app.route('/')
 def Welcome():
-	return'<html><head><title>BiblioDB API</title></head><body><h1>Benvenuti in BiblioDB!</h1><br><p>Per utilizzare l'api e' necessario modificare l'URL.</p><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="VFZK6WR23YBQL"><input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - Il metodo rapido, affidabile e innovativo per pagare e farsi pagare."><img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1"></form></body></html>''
+	return'<html><head><title>BiblioDB API</title></head><body><h1>Benvenuti in BiblioDB!</h1><br><p>Per utilizzare le api bisogna modificare lo URL.</p><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="VFZK6WR23YBQL"><input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - Il metodo rapido, affidabile e innovativo per pagare e farsi pagare."><img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1"></form></body></html>'
 @app.route('/qrcode.png')
 def QrCode():
 	try:
